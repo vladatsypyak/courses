8c8aa6c989a2e631afe4040b10d6545c9d234e65
Vladyslava <vladatsyupyak@gmail.com>
2023-01-20 21:03:15 +0200

    feat: add link to logo

--- src/components/Header/components/Logo/Logo.jsx
+++ src/components/Header/components/Logo/Logo.jsx
@@ -1,8 +1,12 @@
 import "../../Header.css"
+import {Link} from "react-router-dom";
 
 const Logo = ()=>{
     return(
-        <img className={"logo"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScHXkxgYzdD7eE9nrLMLlkkVH9jFkcAnJnzQ&usqp=CAU" alt=""/>
+        <Link to={"/"}>
+            <img className={"logo"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScHXkxgYzdD7eE9nrLMLlkkVH9jFkcAnJnzQ&usqp=CAU" alt=""/>
+
+        </Link>
     )
  }
  export default Logo
