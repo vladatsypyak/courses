eb69c056ecbc7a4934dcc3fd049490e6e69f4659
Vladyslava <vladatsyupyak@gmail.com>
2023-01-24 00:27:24 +0200

    feat: create registration page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CourseCard from "./components/Courses/components/CourseCard/CourseCard";
 import Courses from "./components/Courses/Courses";
 import {Route, Routes} from "react-router-dom"
 import CreateCourse from "./components/CreateCourse/CreateCourse";
+import Registration from "./components/Courses/components/Registration/Registration";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Courses/>}/>
             <Route path="/createCourse" element={<CreateCourse/>}/>
-
+            <Route path="/registration" element={<Registration/>}/>
         </Routes>
     </div>);
 }
---
+++ src/components/Courses/components/Registration/Registration.js
@@ -0,0 +1,38 @@
+import Input from "../../../../common/Input/Input";
+import Button from "../../../../common/Button/Button";
+import {useForm} from "react-hook-form";
+import {useState} from "react";
+
+export default function Registration(){
+    const[name, setName]= useState("")
+    function onNameChange(e) {
+        console.log(e.target.value)
+        setName(e.target.value)
+    }
+    function onEmailChange(e) {
+        console.log(e.target.value)
+        setName(e.target.value)
+    }
+    function onPasswordChange(e) {
+        console.log(e.target.value)
+        setName(e.target.value)
+    }
+    const onSubmit = (data) => {
+
+        console.log(data);
+    }
+    const {register, control, handleSubmit, watch, formState: {errors}} = useForm();
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className={"registration_wrap"}>
+            <div className="registration_flex_wrap">
+                <Input  placeholderText={"name"}  labelText={"Name"} classname={"registration_input"} register={register}
+                        onChange={onNameChange} required/>
+                <Input  placeholderText={"email"}  labelText={"Email"} classname={"registration_input"} register={register}
+                        onChange={onEmailChange} required/>
+                <Input  placeholderText={"password"} inputType={"password"}  labelText={"Password"} classname={"registration_input"} register={register}
+                        onChange={onPasswordChange} required/>
+                <button type={"submit"}>Registration</button>
+            </div>
+        </form>
+    )
+}
